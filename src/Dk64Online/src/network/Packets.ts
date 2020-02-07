e8854f5f5b365c0ec9d8decec4a94f32c4f082f6
spiceywolf <SpiceyWolf.ArchaicSoft@outlook.com>
2020-02-06 22:25:59 -0500

    Commit PMONickPop123's changes

--- src/Dk64Online/src/network/Packets.ts
+++ src/Dk64Online/src/network/Packets.ts
@@ -2,37 +2,39 @@ import { Packet, UDPPacket } from 'modloader64_api/ModLoaderDefaultImpls';
 import { KongData } from './Database';
 
 export class SyncStorage extends Packet {
-  kong: Buffer;
-  game_flags: Buffer;
-  constructor(lobby: string, kong: Buffer, game_flags: Buffer) {
-    super('SyncStorage', 'Dk64Online', lobby, false);
-    this.kong = kong;
-    this.game_flags = game_flags;
-  }
+	game_flags: Buffer;
+	kong: KongData[];
+
+	constructor(lobby: string, game_flags: Buffer, kong: KongData[]) {
+		super('SyncStorage', 'Dk64Online', lobby, false);
+		this.game_flags = game_flags;
+		this.kong = kong;
+	}
 }
 
 export class SyncKong extends Packet {
-  kong: KongData;
-  kong_index: number;
-  constructor(lobby: string, kong: KongData, kong_index: number, persist: boolean) {
-    super('SyncKong', 'Dk64Online', lobby, persist);
-    this.kong = kong;
-    this.kong_index = kong_index;
-  }
+	kong: KongData;
+	kong_index: number;
+
+	constructor(lobby: string, kong: KongData, kong_index: number, persist: boolean) {
+		super('SyncKong', 'Dk64Online', lobby, persist);
+		this.kong = kong;
+		this.kong_index = kong_index;
+	}
 }
 
 export class SyncBuffered extends Packet {
-  value: Buffer;
-  constructor(lobby: string, header: string, value: Buffer, persist: boolean) {
-    super(header, 'Dk64Online', lobby, persist);
-    this.value = value;
-  }
+	value: Buffer;
+	constructor(lobby: string, header: string, value: Buffer, persist: boolean) {
+		super(header, 'Dk64Online', lobby, persist);
+		this.value = value;
+	}
 }
 
 export class SyncNumbered extends Packet {
-  value: number;
-  constructor(lobby: string, header: string, value: number, persist: boolean) {
-    super(header, 'Dk64Online', lobby, persist);
-    this.value = value;
-  }
+	value: number;
+	constructor(lobby: string, header: string, value: number, persist: boolean) {
+		super(header, 'Dk64Online', lobby, persist);
+		this.value = value;
+	}
 }
